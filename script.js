0183c09bf36a91b2cc4df5c2df5ce5fa1fd39e77
imgiridhar <141628169+imgiridhar@users.noreply.github.com>
2025-02-26 12:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const bombCount = 5;
 const balls = [];
 let bombs = [];
 let foods = [];
-const speed = 5;
+const speed = 3;
 const player = document.createElement('div');
 player.className = 'player';
 gameCanvas.appendChild(player);
